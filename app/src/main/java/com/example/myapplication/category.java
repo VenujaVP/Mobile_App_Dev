bf6cb09c258f25555e367d9ef2a0f4e640bbb784
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2024-10-03 21:32:05 +0530

    update8

--- app/src/main/java/com/example/myapplication/category.java
+++ app/src/main/java/com/example/myapplication/category.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class category extends AppCompatActivity {
 
+    private ImageView homeIcon;
+    private ImageView categoryIcon;
+    private ImageView profileIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_category);
+
+        homeIcon = findViewById(R.id.homeIcon);
+        categoryIcon = findViewById(R.id.categoryIcon);
+        profileIcon = findViewById(R.id.profileIcon);
+
+
+        // Set click listeners for icons
+        homeIcon.setOnClickListener(v -> startActivity(new Intent(category.this, MainActivity.class)));
+        categoryIcon.setOnClickListener(v -> startActivity(new Intent(category.this, category.class)));
+        profileIcon.setOnClickListener(v -> startActivity(new Intent(category.this, profile.class)));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
